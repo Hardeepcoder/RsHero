343d08f65c12c09550560852641dd95b66209379
hardeep singh <hardeepsingh@192.168.1.4>
2021-04-25 09:54:29 +0530

    descriptive message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+    fontSize:50, 
+    color:'red', background:'black'}}>Sholay</h1>
+
+<Button variant="contained">Hello World !</Button>
     </div>
+
   );
 }
 
