7e9a57646f438bc0f93c4eadb2ba021126987d2b
hardeepcoder <hardeepphp@yahoo.com>
2021-04-27 16:54:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import { Button } from '@material-ui/core';
+import {Button} from '@material-ui/core'
 function App() {
+  function myfun(){
+    alert('hello sholay');
+  }
   return (
-    <div className="App">
-    <h1 style={{
-    fontSize:50, 
-    color:'red', background:'black'}}>Sholay</h1>
-
-    <Button variant="contained">Hello World... </Button>
-    </div>
+  <div className="App">
+    <h1>Sholay</h1>
+    <Button variant="contained"
+    onClick={myfun}>Click</Button>
+  </div>
 
   );
 }
