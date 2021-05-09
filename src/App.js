5dd28497ab0956c054fd4322a498ab6faecd9f8a
hardeepcoder <hardeepphp@yahoo.com>
2021-05-09 10:33:02 +0530

    validations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import {Button} from '@material-ui/core'
+import {Button, Input} from '@material-ui/core'
+import {useState} from 'react';
 function App() {
+  const [username, setUsername] = useState('');
   function myfun(){
-    alert('hello sholay');
+// now check if username is empty
+    // alert(username);
+
+    if(username===""){
+        alert('please fill the username');
+    }
+    else{
+      alert(username);
+    }
   }
   return (
   <div className="App">
-    <h1>Sholay</h1>
-    <Button variant="contained"
-    onClick={myfun}>Click</Button>
+    <h1>Start with React.js</h1>
+
+    <Input onChange={e => setUsername(e.target.value)}/>
+    <br/><br/>
+<Button onClick={myfun} variant="contained">Get value</Button>
   </div>
 
   );
